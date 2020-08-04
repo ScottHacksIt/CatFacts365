af0eb620f610f48c5c9de2b130ff4169d4c42269
ScottHacksIt <scotthacksit@gmail.com>
2020-08-04 18:47:35 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace CatFacts365
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // send the request
+            string uri = "https://cat-fact.herokuapp.com/facts/random";
+            HttpRequestMessage req = new HttpRequestMessage();
+            req.RequestUri = new Uri(uri);
+            req.Method = HttpMethod.Get;
+            req.Headers.Clear();
+            req.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpClient _client = new HttpClient();            
+            HttpResponseMessage resp = await _client.SendAsync(req);
+            // make sure it was successful
+            resp.EnsureSuccessStatusCode();
+            // extract the json contents from the response
+            string jsonContent = "";
+            if (resp.Content != null)
+                jsonContent = await resp.Content.ReadAsStringAsync();
+            // parse them
+            JToken jsonObj = JsonConvert.DeserializeObject<object>(jsonContent) as JToken;
+            // get that sweet cat fact
+            string catFact = jsonObj["text"].ToString();
+            Console.WriteLine(catFact);
+        }
+    }
+}
