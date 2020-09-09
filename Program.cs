d26c7e40494709ed5d200fa5e72efacdbf3fd327
ScottHacksIt <scotthacksit@gmail.com>
2020-09-09 09:40:56 -0400

    Uses AnimalFactsLib,, but requires the DLLs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using AnimalFactsLib;
 
 namespace CatFacts365
 {
@@ -12,26 +13,10 @@ namespace CatFacts365
     {
         static async Task Main(string[] args)
         {
-            // send the request
-            string uri = "https://cat-fact.herokuapp.com/facts/random";
-            HttpRequestMessage req = new HttpRequestMessage();
-            req.RequestUri = new Uri(uri);
-            req.Method = HttpMethod.Get;
-            req.Headers.Clear();
-            req.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpClient _client = new HttpClient();            
-            HttpResponseMessage resp = await _client.SendAsync(req);
-            // make sure it was successful
-            resp.EnsureSuccessStatusCode();
-            // extract the json contents from the response
-            string jsonContent = "";
-            if (resp.Content != null)
-                jsonContent = await resp.Content.ReadAsStringAsync();
-            // parse them
-            JToken jsonObj = JsonConvert.DeserializeObject<object>(jsonContent) as JToken;
-            // get that sweet cat fact
-            string catFact = jsonObj["text"].ToString();
-            Console.WriteLine(catFact);
+            //AnimalFact factRetriever = new CatFact();
+            AnimalFact factRetriever = new DogFact();
+            string fact = await factRetriever.GetFact();
+            Console.WriteLine(fact);
         }
     }
 }
