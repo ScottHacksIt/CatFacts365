4692e6f1fa7b23329477adc2177209c54c79b141
Scott Hackett <shackett@petpartners.com>
2021-02-09 22:54:08 -0500

    Updating to netcore5.0

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using AnimalFactsLib;
 
 namespace CatFacts365
 {
@@ -13,11 +12,26 @@ namespace CatFacts365
     {
         static async Task Main(string[] args)
         {
-            AnimalFact factRetriever = new CatFact();
-            //AnimalFact factRetriever = new DogFact();
-            //AnimalFact factRetriever = new HorseFact();
-            string fact = await factRetriever.GetFact();
-            Console.WriteLine(fact);
+            // send the request
+            string uri = "https://cat-fact.herokuapp.com/facts/random";
+            HttpRequestMessage req = new HttpRequestMessage();
+            req.RequestUri = new Uri(uri);
+            req.Method = HttpMethod.Get;
+            req.Headers.Clear();
+            req.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpClient _client = new HttpClient();            
+            HttpResponseMessage resp = await _client.SendAsync(req);
+            // make sure it was successful
+            resp.EnsureSuccessStatusCode();
+            // extract the json contents from the response
+            string jsonContent = "";
+            if (resp.Content != null)
+                jsonContent = await resp.Content.ReadAsStringAsync();
+            // parse them
+            JToken jsonObj = JsonConvert.DeserializeObject<object>(jsonContent) as JToken;
+            // get that sweet cat fact
+            string catFact = jsonObj["text"].ToString();
+            Console.WriteLine(catFact);
         }
     }
 }
